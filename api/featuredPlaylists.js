ff30295371cb94107508ddcffb61f80ff9282b3c
Watchout2025 <watchoutofficial2006@gmail.com>
2026-03-11 21:39:28 +0530

    Update featuredPlaylists.js

--- api/featuredPlaylists.js
+++ api/featuredPlaylists.js
@@ -1,6 +1,26 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://www.jiosaavn.com/api.php?__call=content.getFeaturedPlaylists&fetch_from_serialized_files=true&p=1&n=50&api_version=4&_format=json&_marker=0&ctx=wap6dot0");
-  const data = await response.json();
+  try {
+    const response = await fetch("https://www.jiosaavn.com/api.php?__call=content.getFeaturedPlaylists&fetch_from_serialized_files=true&p=1&n=50&api_version=4&_format=json&_marker=0&ctx=wap6dot0",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+          "Accept": "application/json",
+          "Referer": "https://www.jiosaavn.com/"
+        }
+      }
+    );
 
-  res.status(200).json(data);
+    const text = await response.text();
+
+    // convert to JSON safely
+    const data = JSON.parse(text);
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({
+      error: "Charts fetch failed",
+      message: err.message
+    });
+  }
 }
