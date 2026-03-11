4dc5f543d309062706a4301153f73da5de782cf9
Watchout2025 <watchoutofficial2006@gmail.com>
2026-03-11 21:33:36 +0530

    Update charts.js

--- api/charts.js
+++ api/charts.js
@@ -1,6 +1,13 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://www.jiosaavn.com/api.php?__call=content.getCharts&api_version=4&_format=json&_marker=0&ctx=wap6dot0");
-  const data = await response.json();
+  try {
+    const response = await fetch("https://www.jiosaavn.com/api.php?__call=content.getCharts&api_version=4&_format=json&_marker=0&ctx=wap6dot0");
 
-  res.status(200).json(data);
+    const text = await response.text(); // raw response
+
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(text);
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
