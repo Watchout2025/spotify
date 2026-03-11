c3d065d6e901171bce4c468c17f11ae1a14f1198
Watchout2025 <watchoutofficial2006@gmail.com>
2026-03-11 21:39:59 +0530

    Update newReleases.js

--- api/newReleases.js
+++ api/newReleases.js
@@ -1,6 +1,27 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://www.jiosaavn.com/api.php?__call=content.getAlbums&api_version=4&_format=json&_marker=0&n=50&p=1&ctx=wap6dot0");
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      "https://www.jiosaavn.com/api.php?__call=content.getAlbums&api_version=4&_format=json&_marker=0&n=50&p=1&ctx=wap6dot0",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+          "Accept": "application/json",
+          "Referer": "https://www.jiosaavn.com/"
+        }
+      }
+    );
 
-  res.status(200).json(data);
+    const text = await response.text();
+
+    // convert to JSON safely
+    const data = JSON.parse(text);
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({
+      error: "Charts fetch failed",
+      message: err.message
+    });
+  }
 }
